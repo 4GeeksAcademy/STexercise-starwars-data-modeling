6ba689c40c0c683fce632faafe51d972f5843382
StevenSanz <163450220+StevenSanz@users.noreply.github.com>
2024-07-22 10:38:48 +0000

    STxp

--- src/models.py
+++ src/models.py
@@ -12,34 +12,45 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     user_name = Column(String(250), nullable=False)
     email = Column(String(150), nullable=False)
-    login = Column(String(100), nullable =False)
-class Vehicles (Base):
-    __tablename__ = 'vehicles'
+    password = Column(String(100), nullable =False)
+    favorites = relationship("Favorite", back_populates="user")
+
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
     id = Column(Integer, primary_key=True)
     max_velocity = Column(String(250), nullable=False)
     model = Column(String(150), nullable=False)
     capacity = Column(String(20), nullable =False)
+    favorites = relationship("Favorite", back_populates="vehicle")
 
-class Starships (Base):
-    __tablename__ = 'starships'
+class Starship(Base):
+    __tablename__ = 'starship'
     id = Column(Integer, primary_key=True)
     max_velocity = Column(String(250), nullable=False)
     model = Column(String(150), nullable=False)
     capacity = Column(String(20), nullable =False)
+    favorites = relationship("Favorite", back_populates="starship")
 
-class Species (Base):
+class Species(Base):
     __tablename__ = 'species'
     id = Column(Integer, primary_key=True)
     origin = Column(String(250), nullable=False)
     classification = Column(String(150), nullable=False)
     language = Column(String(20), nullable =False)
+    favorites = relationship("Favorite", back_populates="species")
 
 class Fav (Base):
-    __tablename__ = 'fav'
+    __tablename__ = 'favorite'
     id = Column(Integer, primary_key=True)
-    
+    user_id = Column(Integer, ForeignKey('user.id'))
+    fav_vehicle = Column(Integer, ForeignKey('vehicle.id'))
+    fav_starships = Column(String, ForeignKey('starship.id'))
+    fav_species = Column(Integer, ForeignKey('species.id'))
 
-
+    user = relationship("User", back_populates="fav")
+    vehicles = relationship(Vehicle)
+    starships = relationship(Starship)
+    species = relationship(Species)
 # class Person(Base):
 #     __tablename__ = 'person'
 #     id = Column(Integer, primary_key=True)
